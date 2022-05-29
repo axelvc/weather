19455dec99dc8a7179f13ca38467c50ad7136fd3
Axel Vasquez <a.axelvc@gmail.com>
2022-05-29 03:43:53 -0500

    create dial-chart component

---
+++ src/components/DialChart.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  export let total = 100
+  export let value: number
+  export let icon: string
+  export let suffix = ''
+
+  let valuePath: SVGPathElement
+
+  onMount(() => {
+    const length = valuePath.getTotalLength()
+    const filledPercent = value / total
+
+    const dashArray = length
+    const dashOffset = length * (1 - filledPercent)
+
+    valuePath.setAttribute('stroke-dasharray', `${dashArray}`)
+    valuePath.setAttribute('stroke-dashoffset', `${dashOffset}`)
+  })
+</script>
+
+<div class="relative flex justify-center">
+  <span class="absolute bottom-0 left-1/2 -translate-x-1/2">{icon}</span>
+
+  <svg class="w-24 h-24" viewBox="0 0 96 96">
+    <!-- total -->
+    <path
+      class="stroke-slate-200"
+      d="M21,84 C10.07,75.8,3,62.72,3,48C3,23.14,23.14,3,48,3s45,20.14,45,45c0,14.72-7.07,27.8-18,36"
+    />
+
+    <!-- value -->
+    <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle">
+      {value}
+      {suffix}
+    </text>
+
+    <path
+      bind:this={valuePath}
+      class="stroke-slate-900"
+      d="M21,84 C10.07,75.8,3,62.72,3,48C3,23.14,23.14,3,48,3s45,20.14,45,45c0,14.72-7.07,27.8-18,36"
+    />
+  </svg>
+</div>
+
+<style>
+  path {
+    fill: none;
+    stroke-width: 6;
+    stroke-linecap: round;
+    stroke-linecap: round;
+  }
+</style>
