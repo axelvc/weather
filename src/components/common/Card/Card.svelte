dd1d92b2089a5b048e6561994fb7f70f79f09dfe
Axel Vasquez <a.axelvc@gmail.com>
2022-06-01 17:56:14 -0500

    reponsive design

--- src/components/common/Card/Card.svelte
+++ src/components/common/Card/Card.svelte
@@ -2,7 +2,7 @@
   export let title: string
 </script>
 
-<section class={`grid gap-6 m-3 p-6 rounded-lg bg-slate-50 shadow-lg shadow-slate-200 ${$$props.class || ''}`}>
+<section class={`grid gap-6 mx-3 my-6 p-6 rounded-lg bg-slate-50 shadow-lg shadow-slate-200 ${$$props.class || ''}`}>
   <slot name="pre" />
   <h2 class="col-span-full text-xl font-semibold">{title}</h2>
   <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,156 +9,161 @@
   import Time from '@/components/common/Time.svelte'
   import Temp from '@/components/Home/Temp.svelte'
 
-  function fetchWeather(): Promise<Weather> {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(({ coords }) => {
-        getWeather(`${coords.latitude},${coords.longitude}`).then(resolve).catch(reject)
-      }, reject)
-    })
-  }
+  const weatherData = new Promise<Weather>((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      getWeather(`${coords.latitude},${coords.longitude}`).then(resolve).catch(reject)
+    }, reject)
+  })
 </script>
 
-{#await fetchWeather()}
-  <p class="sr-only">Loading</p>
+<main class="md:flex">
+  {#await weatherData}
+    <p class="sr-only">Loading</p>
 
-  <div class="relative animate-pulse grid place-items-center min-h-screen">
-    <div class="absolute top-3 left-3 space-y-2">
-      <div class="rounded-sm w-48 h-5 bg-slate-300" />
-      <div class="rounded-sm w-36 h-4 bg-slate-300" />
-    </div>
-
-    <div class="space-y-2 grid justify-items-center">
-      <div class="w-56 h-40 rounded-md bg-slate-300" />
-      <div class="w-56 h-5 rounded-sm bg-slate-300" />
-      <div class="w-56 h-5 rounded-sm bg-slate-300" />
-      <div class="w-56 h-5 rounded-sm bg-slate-300" />
-    </div>
-  </div>
-{:then { current, location, forecast }}
-  <section class="sticky top-0 grid place-items-center min-h-screen">
-    <div class="absolute top-2 left-3 ">
-      <p class="text-lg">{location.country}, {location.name}</p>
-      <p class="text-sm text-slate-500">
-        Last update at: <Time date={current.lastUpdated} />
-      </p>
-    </div>
-
-    <div class="text-center">
-      <Temp class="text-[10rem] leading-none font-semibold after:text-8xl after:align-top">
-        {current.temp.real.c}
-      </Temp>
-
-      <p>
-        Feels like
-        <Temp>
-          {current.temp.feels.c}
-        </Temp>
-      </p>
-
-      <div>
-        <p class="inline">{current.condition.text}</p>
-        <img src={current.condition.icon} alt="" class="inline w-6" />
+    <div class="relative animate-pulse grid place-items-center md:w-screen h-screen">
+      <div class="absolute top-3 left-3 space-y-2">
+        <div class="rounded-sm w-48 h-5 bg-slate-300" />
+        <div class="rounded-sm w-36 h-4 bg-slate-300" />
       </div>
 
-      <span>
-        🔽
-        <Temp>
-          {forecast.today.temp.min.c}
-        </Temp>
-      </span>
-
-      <span>
-        🔼
-        <Temp>
-          {forecast.today.temp.max.c}
-        </Temp>
-      </span>
+      <div class="space-y-2 grid justify-items-center">
+        <div class="w-56 h-40 rounded-md bg-slate-300" />
+        <div class="w-56 h-5 rounded-sm bg-slate-300" />
+        <div class="w-56 h-5 rounded-sm bg-slate-300" />
+        <div class="w-56 h-5 rounded-sm bg-slate-300" />
+      </div>
     </div>
-  </section>
-
-  <div class="-translate-y-20 bg-slate-100">
-    <Card title="Today" class="grid-cols-2 shadow-[0_-20px_50px_-15px]">
-      <div slot="pre" class="col-span-full h-8">
-        <div class="w-16 h-0.5 mx-auto mb-2 rounded bg-slate-300 " />
-        <div class="w-8 h-0.5 mx-auto rounded bg-slate-300" />
+  {:then { current, location, forecast }}
+    <section class="sticky top-0 grid place-items-center md:w-screen h-screen">
+      <div class="absolute top-2 left-3 ">
+        <p class="text-lg">{location.country}, {location.name}</p>
+        <p class="text-sm text-slate-500">
+          Last update at: <Time date={current.lastUpdated} />
+        </p>
       </div>
 
-      <CardSection title="Cloud Cover" class="grid gap-1 justify-center text-center">
-        <DialChart value={current.cloud} icon="☁️" suffix="%" />
-      </CardSection>
+      <div class="text-center sm:w-96">
+        <Temp class="text-[10rem] leading-none font-semibold after:absolute after:text-8xl after:align-top">
+          {current.temp.real.c}
+        </Temp>
 
-      <CardSection title="Humidity" class="grid gap-1 justify-center text-center">
-        <DialChart value={current.humidity} icon="💧️" suffix="%" />
-      </CardSection>
+        <p>
+          Feels like
+          <Temp>
+            {current.temp.feels.c}
+          </Temp>
+        </p>
 
-      <CardSection title="UV Index" class="grid gap-1 justify-center text-center">
-        <DialChart value={current.uvIndex} total={11} icon="💡" />
-      </CardSection>
+        <div>
+          <p class="inline">{current.condition.text}</p>
+          <img src={current.condition.icon} alt="" class="inline w-6" />
+        </div>
 
-      <CardSection title="Wind" class="grid gap-1 justify-center text-center">
-        <Compass velocity={current.wind.kph} direction={current.wind.direction} />
-      </CardSection>
-    </Card>
+        <span>
+          🔽
+          <Temp>{forecast.today.temp.min.c}</Temp>
+        </span>
 
-    <Card title="Forecast">
-      <CardSection title="Next Days" class="grid">
-        <Forecast data={forecast.days} formatOptions={{ weekday: 'short' }} />
-      </CardSection>
+        <span>
+          🔼
+          <Temp>{forecast.today.temp.max.c} /></Temp>
+        </span>
+      </div>
+    </section>
+  {/await}
 
-      <hr class="border-slate-200" />
+  <div class=" w-full md:max-w-lg -translate-y-20 sm:translate-y-0">
+    {#await weatherData}
+      <div class="animate-pulse">
+        <div class="rounded-sm aspect-video mx-3 my-6 bg-slate-300" />
+        <div class="rounded-sm aspect-video mx-3 my-6 bg-slate-300" />
+        <div class="rounded-sm aspect-video mx-3 my-6 bg-slate-300" />
+        <div class="rounded-sm aspect-video mx-3 my-6 bg-slate-300" />
+      </div>
+    {:then { current, forecast }}
+      <Card title="Today" class="grid-cols-2 shadow-[0_-20px_50px_-15px]">
+        <div slot="pre" class="md:hidden col-span-full h-8">
+          <div class="w-16 h-0.5 mx-auto mb-2 rounded bg-slate-300 " />
+          <div class="w-8 h-0.5 mx-auto rounded bg-slate-300" />
+        </div>
 
-      <CardSection title="Next Hours" class="grid">
-        <Forecast data={forecast.hours} formatOptions={{ hour: 'numeric' }} />
-      </CardSection>
-    </Card>
+        <CardSection title="Cloud Cover" class="grid gap-1 justify-center text-center">
+          <DialChart value={current.cloud} icon="☁️" suffix="%" />
+        </CardSection>
 
-    <Card title="Sun & Moon" class="grid-cols-2">
-      <CardSection title="Sunrise">
-        ☀️
-        <Time date={forecast.today.astro.sunrise} />
-      </CardSection>
-      <CardSection title="Sunset">
-        ☀️
-        <Time date={forecast.today.astro.sunset} />
-      </CardSection>
+        <CardSection title="Humidity" class="grid gap-1 justify-center text-center">
+          <DialChart value={current.humidity} icon="💧️" suffix="%" />
+        </CardSection>
 
-      <hr class="col-span-full border-slate-200" />
+        <CardSection title="UV Index" class="grid gap-1 justify-center text-center">
+          <DialChart value={current.uvIndex} total={11} icon="💡" />
+        </CardSection>
 
-      <CardSection title="Monrise">
-        ☀️
-        <Time date={forecast.today.astro.moonrise} />
-      </CardSection>
-      <CardSection title="Moonset">
-        ☀️
-        <Time date={forecast.today.astro.moonset} />
-      </CardSection>
-      <CardSection title="Moon Phase">
-        {forecast.today.astro.moonPhase}
-      </CardSection>
-      <CardSection title="Moon Illumination">
-        {forecast.today.astro.moonIllumination}%
-      </CardSection>
-    </Card>
+        <CardSection title="Wind" class="grid gap-1 justify-center text-center">
+          <Compass velocity={current.wind.kph} direction={current.wind.direction} />
+        </CardSection>
+      </Card>
 
-    <Card title="More info" class="grid-cols-2">
-      <CardSection title="Dew point">
-        <Temp>{forecast.today.temp.dewPoint.c}</Temp>
-      </CardSection>
-      <CardSection title="Visibility">
-        {current.visibility.km} km
-      </CardSection>
-      <CardSection title="Pressure">
-        {current.pressure.mb} mb
-      </CardSection>
-      <CardSection title="Precipitation ">
-        {current.precipitation.mm} mm
-      </CardSection>
-      <CardSection title="Chance of snow">
-        {forecast.today.snowChange}%
-      </CardSection>
-      <CardSection title="Chance of rain">
-        {forecast.today.rainChange}%
-      </CardSection>
-    </Card>
+      <Card title="Forecast">
+        <CardSection title="Next Days" class="grid">
+          <Forecast data={forecast.days} formatOptions={{ weekday: 'short' }} />
+        </CardSection>
+
+        <hr class="border-slate-200" />
+
+        <CardSection title="Next Hours" class="grid">
+          <Forecast data={forecast.hours} formatOptions={{ hour: 'numeric' }} />
+        </CardSection>
+      </Card>
+
+      <Card title="Sun & Moon" class="grid-cols-2">
+        <CardSection title="Sunrise">
+          ☀️
+          <Time date={forecast.today.astro.sunrise} />
+        </CardSection>
+        <CardSection title="Sunset">
+          ☀️
+          <Time date={forecast.today.astro.sunset} />
+        </CardSection>
+
+        <hr class="col-span-full border-slate-200" />
+
+        <CardSection title="Monrise">
+          ☀️
+          <Time date={forecast.today.astro.moonrise} />
+        </CardSection>
+        <CardSection title="Moonset">
+          ☀️
+          <Time date={forecast.today.astro.moonset} />
+        </CardSection>
+        <CardSection title="Moon Phase">
+          {forecast.today.astro.moonPhase}
+        </CardSection>
+        <CardSection title="Moon Illumination">
+          {forecast.today.astro.moonIllumination}%
+        </CardSection>
+      </Card>
+
+      <Card title="More info" class="grid-cols-2">
+        <CardSection title="Dew point">
+          <Temp>{forecast.today.temp.dewPoint.c}</Temp>
+        </CardSection>
+        <CardSection title="Visibility">
+          {current.visibility.km} km
+        </CardSection>
+        <CardSection title="Pressure">
+          {current.pressure.mb} mb
+        </CardSection>
+        <CardSection title="Precipitation ">
+          {current.precipitation.mm} mm
+        </CardSection>
+        <CardSection title="Chance of snow">
+          {forecast.today.snowChange}%
+        </CardSection>
+        <CardSection title="Chance of rain">
+          {forecast.today.rainChange}%
+        </CardSection>
+      </Card>
+    {/await}
   </div>
-{/await}
+</main>
