720095b5acaee5fdfeffe040684870f88eb52c88
Axel Vasquez <a.axelvc@gmail.com>
2022-05-31 19:16:15 -0500

    add landing structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,61 +26,85 @@
   }
 </script>
 
-<Card title="Today" class="grid-cols-2">
-  <CardSection title="Cloud Cover" class="grid gap-1 justify-center text-center">
-    <DialChart value={90} icon="☁️" suffix="%" />
-  </CardSection>
+<section class="sticky top-0 -z-10 grid place-items-center min-h-screen">
+  <div class="absolute top-2 left-3 ">
+    <p class="text-lg">Location</p>
+    <p class="text-sm text-slate-500">Last update at 12:30</p>
+  </div>
 
-  <CardSection title="Humidity" class="grid gap-1 justify-center text-center">
-    <DialChart value={65} icon="💧️" suffix="%" />
-  </CardSection>
+  <div class="text-center my-16">
+    <span class="text-[10rem] leading-none font-semibold">
+      13<span class="text-8xl align-top">°</span>
+    </span>
+    <p>Feels like 12.8°</p>
+    <p>Partly cloudy ☁️</p>
+    <span>🔽 12°</span>
+    <span>🔼 14°</span>
+  </div>
+</section>
 
-  <CardSection title="UV Index" class="grid gap-1 justify-center text-center">
-    <DialChart value={2} total={11} icon="💡" />
-  </CardSection>
+<div class="-translate-y-20 bg-slate-100">
+  <Card title="Today" class="grid-cols-2 shadow-[0_-20px_50px_-15px]">
+    <div slot="pre" class="col-span-full h-8">
+      <div class="w-16 h-0.5 mx-auto mb-2 rounded bg-slate-300 " />
+      <div class="w-8 h-0.5 mx-auto rounded bg-slate-300" />
+    </div>
 
-  <CardSection title="Wind" class="grid gap-1 justify-center text-center">
-    <Compass velocity={22} direction={'N'} />
-  </CardSection>
-</Card>
+    <CardSection title="Cloud Cover" class="grid gap-1 justify-center text-center">
+      <DialChart value={90} icon="☁️" suffix="%" />
+    </CardSection>
 
-<Card title="Forecast">
-  <CardSection title="Next Days" class="grid">
-    <Forecast data={forecastData.days} />
-  </CardSection>
+    <CardSection title="Humidity" class="grid gap-1 justify-center text-center">
+      <DialChart value={65} icon="💧️" suffix="%" />
+    </CardSection>
 
-  <hr class="border-slate-200" />
+    <CardSection title="UV Index" class="grid gap-1 justify-center text-center">
+      <DialChart value={2} total={11} icon="💡" />
+    </CardSection>
 
-  <CardSection title="Next Hours" class="grid">
-    <Forecast data={forecastData.hours} />
-  </CardSection>
-</Card>
+    <CardSection title="Wind" class="grid gap-1 justify-center text-center">
+      <Compass velocity={22} direction={'N'} />
+    </CardSection>
+  </Card>
 
-<Card title="Sun & Moon" class="grid-cols-2">
-  <CardSection title="Sunrise">
-    ☀️ <time datetime={new Date().toISOString()}>6:57 AM</time>
-  </CardSection>
-  <CardSection title="Sunset">
-    ☀️ <time datetime={new Date().toISOString()}>6:57 AM</time>
-  </CardSection>
+  <Card title="Forecast">
+    <CardSection title="Next Days" class="grid">
+      <Forecast data={forecastData.days} />
+    </CardSection>
 
-  <hr class="col-span-full border-slate-200" />
+    <hr class="border-slate-200" />
 
-  <CardSection title="Monrise">
-    ☀️ <time datetime={new Date().toISOString()}>6:57 AM</time>
-  </CardSection>
-  <CardSection title="Moonset">
-    ☀️ <time datetime={new Date().toISOString()}>6:57 AM</time>
-  </CardSection>
-  <CardSection title="Moon Illumination">15%</CardSection>
-  <CardSection title="Moon Phase">Warning crecent</CardSection>
-</Card>
+    <CardSection title="Next Hours" class="grid">
+      <Forecast data={forecastData.hours} />
+    </CardSection>
+  </Card>
 
-<Card title="More info" class="grid-cols-2">
-  <CardSection title="Dew point">9°</CardSection>
-  <CardSection title="Visibility">16.10km</CardSection>
-  <CardSection title="Pressure">1023mb</CardSection>
-  <CardSection title="Precipitation ">0mm</CardSection>
-  <CardSection title="Chance of snow">0%</CardSection>
-  <CardSection title="Chance of rain">0%</CardSection>
-</Card>
+  <Card title="Sun & Moon" class="grid-cols-2">
+    <CardSection title="Sunrise">
+      ☀️ <time datetime={new Date().toISOString()}>6:57 AM</time>
+    </CardSection>
+    <CardSection title="Sunset">
+      ☀️ <time datetime={new Date().toISOString()}>6:57 AM</time>
+    </CardSection>
+
+    <hr class="col-span-full border-slate-200" />
+
+    <CardSection title="Monrise">
+      ☀️ <time datetime={new Date().toISOString()}>6:57 AM</time>
+    </CardSection>
+    <CardSection title="Moonset">
+      ☀️ <time datetime={new Date().toISOString()}>6:57 AM</time>
+    </CardSection>
+    <CardSection title="Moon Illumination">15%</CardSection>
+    <CardSection title="Moon Phase">Warning crecent</CardSection>
+  </Card>
+
+  <Card title="More info" class="grid-cols-2">
+    <CardSection title="Dew point">9°</CardSection>
+    <CardSection title="Visibility">16.10km</CardSection>
+    <CardSection title="Pressure">1023mb</CardSection>
+    <CardSection title="Precipitation ">0mm</CardSection>
+    <CardSection title="Chance of snow">0%</CardSection>
+    <CardSection title="Chance of rain">0%</CardSection>
+  </Card>
+</div>
